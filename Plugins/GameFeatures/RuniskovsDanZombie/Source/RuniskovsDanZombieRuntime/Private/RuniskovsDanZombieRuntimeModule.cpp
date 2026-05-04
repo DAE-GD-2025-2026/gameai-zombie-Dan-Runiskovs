92c5d4cf4f4baae2d869adb9ad01ea3a477b4559
Dan Runiskovs <dan.runiskovs@student.howest.be>
2026-05-04 09:55:25 +0200

    Setup for Exam Project

---
+++ Plugins/GameFeatures/RuniskovsDanZombie/Source/RuniskovsDanZombieRuntime/Private/RuniskovsDanZombieRuntimeModule.cpp
@@ -0,0 +1,21 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RuniskovsDanZombieRuntimeModule.h"
+
+#define LOCTEXT_NAMESPACE "FRuniskovsDanZombieRuntimeModule"
+
+void FRuniskovsDanZombieRuntimeModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory;
+	// the exact timing is specified in the .uplugin file per-module
+}
+
+void FRuniskovsDanZombieRuntimeModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.
+	// For modules that support dynamic reloading, we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FRuniskovsDanZombieRuntimeModule, RuniskovsDanZombieRuntime)
